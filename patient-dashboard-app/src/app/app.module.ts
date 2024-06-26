fbe00e4c23a20d8cccb0085c7e2e7512726413e4
Taylor Pekarek <tpekarek@sondermind.com>
2024-06-26 15:58:03 -0600

    Import material components needed for the patient form

--- patient-dashboard-app/src/app/app.module.ts
+++ patient-dashboard-app/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +28,12 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
