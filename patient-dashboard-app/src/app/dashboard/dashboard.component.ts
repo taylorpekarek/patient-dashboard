260e5474369a6e5e5e2b078766de34d9dfeb0e25
Taylor Pekarek <tpekarek@sondermind.com>
2024-06-26 15:20:41 -0600

    Add a button to the dashboard to open the form component

--- patient-dashboard-app/src/app/dashboard/dashboard.component.ts
+++ patient-dashboard-app/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  showPatientForm: boolean = false;
 
+  constructor() {}
+
+  ngOnInit(): void {
+      // fetch data here
+  }
+
+  openPatientForm(patientToUpdate?: any) {
+    this.showPatientForm = true;
+    // Handle opening form in either add new mode or edit mode depending on if param is passed in or not
+  }
 }
