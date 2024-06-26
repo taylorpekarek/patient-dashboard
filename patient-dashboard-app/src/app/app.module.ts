1bf90eacc7fb8c2939058dc82a88679d52cbd26b
Taylor Pekarek <tpekarek@sondermind.com>
2024-06-26 14:07:35 -0600

    Update angular material package

--- patient-dashboard-app/src/app/app.module.ts
+++ patient-dashboard-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PatientFormComponent } from './patient-form/patient-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PatientFormComponent } from './patient-form/patient-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
