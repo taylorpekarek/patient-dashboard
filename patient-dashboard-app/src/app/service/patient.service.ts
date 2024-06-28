b436b7d095d738ab15c799f0185de5f2bcd56ac6
Taylor Pekarek <tpekarek@sondermind.com>
2024-06-27 21:22:22 -0600

    Write method to get patients and their addresses and convert them to my IPatient interface

--- patient-dashboard-app/src/app/service/patient.service.ts
+++ patient-dashboard-app/src/app/service/patient.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData, doc, docData } from '@angular/fire/firestore';
+import { IAddress, IPatient } from '../models/patient.model';
+import { Observable, Timestamp, combineLatest, map, switchMap } from 'rxjs';
 
+const PATIENT_COLLECTION = 'patient';
+const ADDRESS_COLLECTION = 'address';
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
+  patientsCollection;
 
-  constructor() { }
+  constructor(private firestore: Firestore) {
+    this.patientsCollection = collection(this.firestore, PATIENT_COLLECTION);
+  }
+
+  getPatients(): Observable<IPatient[]> {
+    const patients = collectionData(this.patientsCollection, { idField: 'id' });// as Observable<IPatient[]>;
+
+    return patients.pipe(
+      switchMap(patientData => {
+        const patientObservables = patientData.map(patient => {
+
+          const convertedPatient = { ...patient, dateOfBirth: patient['dateOfBirth'].toDate() } as IPatient;
+
+          return this.getPatientAddresses(convertedPatient.id).pipe(
+            map(addresses => ({
+              ...convertedPatient,
+              addresses
+            }))
+          )
+      });
+
+        return combineLatest(patientObservables);
+      })
+    );
+  }
+
+  getPatientAddresses(patientId: string) {
+    const addressesCollection = collection(this.patientsCollection, patientId, 'address');
+    return collectionData(addressesCollection, { idField: 'id' }) as Observable<IAddress[]>;
+  }
 }
