c5c2edb5388378e760d9b1d2d8fbc4a285a71ecc
Taylor Pekarek <tpekarek@sondermind.com>
2024-06-27 18:27:00 -0600

    Create firebase service class and add environment file with firebase config

---
+++ patient-dashboard-app/src/app/environment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA0V38L47RHhk-sgkUDJ1q5y1rlCMIbuYI",
+  authDomain: "patient-dashboard-app-db.firebaseapp.com",
+  databaseURL: "https://patient-dashboard-app-db-default-rtdb.firebaseio.com",
+  projectId: "patient-dashboard-app-db",
+  storageBucket: "patient-dashboard-app-db.appspot.com",
+  messagingSenderId: "555184908669",
+  appId: "1:555184908669:web:cc07e5709d8dcc220e0414"
+};
